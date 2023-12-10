26fc432d4b272db34e5fc981115b454194b70372
AzuliBean <theretrogun@gmail.com>
2023-12-10 11:42:55 +0100

    started the soroity world hand in

--- sorority_world.py
+++ sorority_world.py
@@ -1,10 +1,55 @@
+import sys
+
 members = ['Augusta','Charmain','Billie','Mandy','Charlotte','Lesley']
 likes = {'Augusta':['Charmain','Billie','Mandy','Charlotte','Lesley'],'Charmain':['Augusta','Mandy'],'Billie':['Augusta','Charmain','Lesley',],'Mandy':['Charlotte','Billie','Augusta'],'Lesley':['Billie']}
 rooms_with = {'Augusta':'Charmain','Charmain':'Augusta','Billie':'Lesley','Lesley': 'Billie','Charlotte':'Mandy','Mandy':'Charlotte'}
 
+def check_datatype(data, datatype):
+    
+    def is_Integer(data):
+        #if the object "data" is a integer the it will return true if not it will return false
+        return isinstance(data, int)
+
+    def is_Float(data):
+        
+        return isinstance(data, float)
+
+    def is_Text(data):
+        
+        return isinstance(data, str)
+    
+    if not datatype in "IFT":
+        sys.exit(f"Error processing data. Expected a: {datatype} but got : {data}")
+    #tests if you get a a error
+    try:
+       
+        if datatype == "I" and is_Integer(int(data)):
+            return True
+        
+        elif datatype == "F" and is_Float(float(data)):
+            return True
+
+        elif datatype == "T" and is_Text(data):
+            return True
+    #if there is a error then run the code under 
+    except:
+       return False
+
+
+
+def meny_val_ett(members):
+    print(members)
+
+def meny_val_tva(members,likes):
+    print()
+
+def meny_val_tre(members,rooms_with):
+    print()
+
+
 def check_interval(data,low,high):
     # Om det inte finns någon data: returnera False
-    if is_integer(data):
+    if is_Integer(data):
         if int(data) in range (low,high+1):
             return True
     return False
