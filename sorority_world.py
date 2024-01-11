3f910b467633ef6ef73cb83f85c6ad3d63768394
AzuliBean <theretrogun@gmail.com>
2024-01-11 15:01:48 +0100

    updated sorority

--- sorority_world.py
+++ sorority_world.py
@@ -4,76 +4,47 @@ members = ['Augusta','Charmain','Billie','Mandy','Charlotte','Lesley']
 likes = {'Augusta':['Charmain','Billie','Mandy','Charlotte','Lesley'],'Charmain':['Augusta','Mandy'],'Billie':['Augusta','Charmain','Lesley',],'Mandy':['Charlotte','Billie','Augusta'],'Lesley':['Billie']}
 rooms_with = {'Augusta':'Charmain','Charmain':'Augusta','Billie':'Lesley','Lesley': 'Billie','Charlotte':'Mandy','Mandy':'Charlotte'}
 
-
-
-
-
 def meny_val_ett(members):
-    for member in members:
-        print(members)
+    for x in members:
+        print(x)
 
 def meny_val_tva(members, likes):
-    find = input("Who is it that you're looking for: ")
+    find = input("Vem är det du letar efter: ")
 
     if find not in members:
-        print("f{find} is not a member of the association")
+        print(f"{find} är inte medlem i föreningen")
         return
     
     liked = likes.get(find, [])
 
     if not liked:
-        print(f"{find} doesn't like anyone")
+        print(f"{find} gillar ingen")
     elif set(liked) == set(members) - {find}:
-        print(f"{find} likes everyone(except themselves).")
+        print(f"{find} gillar alla.")
     else:
-        print(f"{find} likes: {', '.join(liked)}")
+        print(f"{find} gillar: {', '.join(liked)}")
 
 def meny_val_tre(members, rooms_with):
-    find = input("Who is it that you're looking for")
+    find = input("Vem är det du letar efter: ")
 
     if find not in members:
-        print("f{find} is not a member of the association")
+        print(f"{find} är inte medlem i föreningen")
         return
     
     roommate = rooms_with.get(find, None)
 
     if roommate is not None:
-        print(f"{find} shares a room with {roommate}")
+        print(f"{find} delar rum med {roommate}")
     else:
-        print(f"{find} does not share a room with anyone.")
+        print(f"{find} delar inte rum med någon.")
 
-
-
-def check_datatype(data, datatype):
-    
-    def is_Integer(data):
-        # if the object "data" is an integer, then return True; otherwise, return False
-        return isinstance(data, int)
-
-    def is_Float(data):
-        return isinstance(data, float)
-
-    def is_Text(data):
-        return isinstance(data, str)
-
-    if not datatype in "IFT":
-        sys.exit(f"Error processing data. Expected a: {datatype} but got : {data}")
-
-    # tests if you get an error
+def is_Integer(num):
     try:
-        if datatype == "I" and is_Integer(int(data)):
-            return True
-
-        elif datatype == "F" and is_Float(float(data)):
-            return True
-
-        elif datatype == "T" and is_Text(data):
-            return True
-    # if there is an error, then run the code under 
-    except:
+        int(num)
+        return True
+    except ValueError:
         return False
 
-# The check_interval function from the first code snippet remains unchanged
 def check_interval(data, low, high):
     if is_Integer(data):
         if int(data) in range(low, high + 1):
